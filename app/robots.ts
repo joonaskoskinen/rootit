2ee61121ca49e370fb31454e9c9a1fdbe724cb97
joonaskoskinen <149376043+joonaskoskinen@users.noreply.github.com>
2026-04-29 20:12:30 +0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,25 @@
 import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
+  const baseUrl = 'https://rootit.fi'
+  
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/dashboard/', '/auth/'],
-    },
-    sitemap: 'https://rootit.fi/sitemap.xml',
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/dashboard/', '/auth/callback', '/auth/error'],
+      },
+      {
+        userAgent: 'GPTBot',
+        disallow: ['/'],
+      },
+      {
+        userAgent: 'CCBot',
+        disallow: ['/'],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
   }
 }
