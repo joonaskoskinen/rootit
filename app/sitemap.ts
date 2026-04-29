713a5d46e7e0a626d70a05d86e7601cd9266a6b7
joonaskoskinen <149376043+joonaskoskinen@users.noreply.github.com>
2026-04-29 20:10:01 +0300

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,22 +11,22 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: `${baseUrl}/#services`,
+      url: `${baseUrl}/demo`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/#pricing`,
+      url: `${baseUrl}/auth/login`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.8,
+      priority: 0.5,
     },
     {
-      url: `${baseUrl}/#faq`,
+      url: `${baseUrl}/auth/sign-up`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.7,
+      priority: 0.5,
     },
   ]
 }
